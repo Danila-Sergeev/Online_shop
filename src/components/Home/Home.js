f0440c583f48a416f1abc69768539959f0f03ae2
Sergeev Danila <Danilasergeev2003@icloud.com>
2023-07-26 22:05:09 +0300

    add 2 boxes and bar plus routing on home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,72 @@
 import homeStiles from "./Home.module.css";
+import { NavLink } from "react-router-dom";
 import Slider from "../Slider/Slider";
+import hoveStyles from "./Home.module.css";
+import data from "../../utils/data";
+import Card from "../Card/Card";
 export default function Home() {
   return (
-    <div>
+    <div className={homeStiles.home}>
       <Slider />
+      <div className={homeStiles.textBox}>
+        <p className={hoveStyles.text}>PARKS AND RECREATION</p>
+        <NavLink to="/shop" className={homeStiles.link}>
+          SHOP NEW
+        </NavLink>
+      </div>
+
+      <div className={homeStiles.photosBox}>
+        <NavLink to="/shop" className={homeStiles.photoBox}>
+          {data.map((obj) => {
+            if (obj.id === 9) {
+              return (
+                <img
+                  className={homeStiles.img}
+                  src={obj.image}
+                  alt={obj.name}
+                ></img>
+              );
+            }
+          })}
+          <div className={hoveStyles.titleBox}>
+            <p className={hoveStyles.text}> LEANING IN</p>
+            <NavLink to="/shop" className={homeStiles.link}>
+              SHOP NEW
+            </NavLink>
+          </div>
+        </NavLink>
+        <NavLink to="/shop" className={homeStiles.photoBox}>
+          {data.map((obj) => {
+            if (obj.id === 7) {
+              return (
+                <img
+                  className={homeStiles.img}
+                  src={obj.image}
+                  alt={obj.name}
+                ></img>
+              );
+            }
+          })}
+
+          <div className={hoveStyles.titleBox}>
+            <p className={hoveStyles.text}> LEANING IN</p>
+            <NavLink to="/shop" className={homeStiles.link}>
+              SHOP NEW
+            </NavLink>
+          </div>
+        </NavLink>
+      </div>
+      <NavLink to="/shop" className={homeStiles.bar}>
+        {data.map((obj) => {
+          if (obj.id <= 3) {
+            return (
+              <div className={homeStiles.card}>
+                <Card key={obj.image} {...obj}></Card>
+              </div>
+            );
+          }
+        })}
+      </NavLink>
     </div>
   );
 }
