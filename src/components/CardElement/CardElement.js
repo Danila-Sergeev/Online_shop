ca18685df73dfad5fd693dd32106d22f57977e08
Sergeev Danila <Danilasergeev2003@icloud.com>
2023-08-03 17:27:10 +0300

    fix deleting items

--- src/components/CardElement/CardElement.js
+++ src/components/CardElement/CardElement.js
@@ -5,9 +5,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { REMOVE_ITEM, RESET_ITEM } from "../../services/actions/cardItems";
 export default function CardElement({ onClose }) {
   const Carditems = useSelector((store) => store.cardItem.items);
+  const CarditemsUnq = useSelector((store) => store.cardItem.unq);
+  console.log(CarditemsUnq);
+  console.log(Carditems);
   const dispatch = useDispatch();
-  const removeItem = (id4) => {
-    dispatch({ type: REMOVE_ITEM, id4 });
+  const removeItem = (id) => {
+    dispatch({ type: REMOVE_ITEM, id });
   };
   const resetItem = () => {
     dispatch({ type: RESET_ITEM });
@@ -22,10 +25,7 @@ export default function CardElement({ onClose }) {
     Carditems.forEach((item) => (price += item.props.price));
     return price ? price : 0;
   }, [Carditems]);
-  let unq = Carditems.filter(
-    (elem, index) =>
-      Carditems.findIndex((obj) => obj.props.id === elem.props.id) === index
-  );
+
   return (
     <div className={cardElementStyles.main}>
       <button
@@ -47,21 +47,21 @@ export default function CardElement({ onClose }) {
         });
       })}
 
-      {unq.map((unique) => {
+      {CarditemsUnq.map((unique) => {
         let uniqueItem = unique.props;
         return (
-          <div className={cardElementStyles.box} key={unq.id4}>
+          <div className={cardElementStyles.box} key={CarditemsUnq.id4}>
             <img className={cardElementStyles.img} src={uniqueItem.image}></img>
             <div className={cardElementStyles.info}>
               <p className={cardElementStyles.name}>{uniqueItem.name}</p>
               <p className={cardElementStyles.price}>{uniqueItem.price} $</p>
             </div>
             <button
-              onClick={() => removeItem(Carditems.id4)}
+              onClick={() => removeItem(uniqueItem.id)}
               className={cardElementStyles.btn}
             >
               <img
-                onClick={() => removeItem(unq.id4)}
+                onClick={() => removeItem(uniqueItem.id)}
                 className={cardElementStyles.bascet}
                 src="https://avatars.mds.yandex.net/i?id=13c7dd9528c82e0e90dad5bbd588771eb5d05453-9043236-images-thumbs&n=13"
               ></img>
--- src/services/reducers/cardItems.js
+++ src/services/reducers/cardItems.js
@@ -2,6 +2,7 @@ import { REMOVE_ITEM, RESET_ITEM, ADD_ITEM } from "../actions/cardItems";
 
 const initialState = {
   items: [],
+  unq: [],
 };
 export const cardItemReducer = (state = initialState, action) => {
   switch (action.type) {
@@ -9,18 +10,25 @@ export const cardItemReducer = (state = initialState, action) => {
       return {
         ...state,
         items: [...state.items, action.item],
+        unq: state.items.filter(
+          (elem, index) =>
+            state.items.findIndex((obj) => obj.props.id === elem.props.id) ===
+            index
+        ),
       };
     }
     case RESET_ITEM: {
       return {
         ...state,
         items: [],
+        unq: [],
       };
     }
     case REMOVE_ITEM: {
       return {
         ...state,
-        items: state.items.filter((item) => item.id4 !== action.id4),
+        items: state.items.filter((item) => item.props.id !== action.id),
+        unq: state.unq.filter((item) => item.props.id !== action.id),
       };
     }
     default: {
